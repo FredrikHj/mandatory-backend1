c04175fc1c66eb548be39aacc3f6e93af2e4dc48
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2019-06-02 00:17:11 +0200

    Rooms file is fixed with its structure and its id

--- server.js
+++ server.js
@@ -1,18 +1,65 @@
 // Övning med routes     
 const express = require('express');
-const socket = require('socket.io');
-
-
 const app = express();
 app.use(express.json());
-
-
 const fileSystem = require('fs');
+
+const socket = require('socket.io');
+
 const port = 3001;
 const server = app.listen(port, () =>  console.log(`Chatserver is listening on port ${port}!`));
+let roomSetting =  require('./server/roomSetting.json');
 
-let chatRooms = require('./server/ChatRoom.json');
-let chatMess = require('./server/ChatMess.json');
+// Create a chatRoom
+let roomId  = () => { 
+    for (let index = 0; index < roomSetting.length; index++) {
+        let idMax = roomSetting[index];
+        countID = idMax.id;
+    }
+    // Get the last id in my arr of movies
+    console.log('Id innan förändring');
+    console.log(parseInt(countID));
+    
+    countID++;
+    console.log('44');
+    console.log(countID);
+    
+    return countID;
+}
+app.post('/NewRoom', (req, res) => {
+    let roomSettingObj = {
+        id: JSON.stringify(roomId()),
+        name: req.body.roomName,
+    }
+    roomSetting.push(roomSettingObj);
+    let roomBody = {
+        config: {
+            id: JSON.stringify(roomId() - 1), // -1 = Give the room correct ID
+            name: req.body.roomName,
+            userTyped: [],
+            "status": "",
+            "content-type": "",
+        },
+        messegnes: [],
+    } 
+
+    console.log(roomBody);
+    // Save some setting of the chatRooms
+    fileSystem.writeFile('./server/roomSetting.json', JSON.stringify(roomSetting //debugging
+        , null, 2
+        ), function(err) {
+            
+            console.log(err);     
+        });    
+
+    // Save the new room into a json files named as room + its ID
+    fileSystem.writeFile('./server/rooms/ChatRoom' + roomBody.config.id+ '.json', JSON.stringify(roomBody //debugging
+        , null, 2
+        ), function(err) {
+            
+            console.log(err);     
+        });    
+});
 
 // Set id for the mess ==================
 let countID = 0;
@@ -37,8 +84,11 @@ const io = socket(server);
 io.on('connection', (socket) => {
     console.log('Anslutning upprättad', socket.id);
 
-    // Send all the mess on the server at once the client is fired up
-    io.sockets.emit('messegnes', chatMess); 
+    
+    // Send all messegnes on the server at once the client is open
+    
+    io.sockets.emit('messegnes', chatMess);
+    
 
     // Listen on newMessegnes and send it to all the client
     socket.on('newMessegnes', (data) => {
@@ -61,21 +111,20 @@ io.on('connection', (socket) => {
             ), function(err) {
                 
                 console.log(err);     
-            });
-        
-            // Send the mess on server at once ther is any incommin mess from the client
-            io.sockets.emit('newMessegnes', chatMessObj);        
+            });       
+        // Send the mess on server at once ther is any incommin mess from the client
+        io.sockets.emit('newMessegnes', chatMessObj);            
     });
 });
-
 let chatroomCountID = 0;
 
 let messCountID = 0;
 /*
 // Gets chattmess =================================================
- app.get('/', (req, res) => {    
+app.get('/', (req, res) => {    
     res.send(movieList);
 });
 
 // Create a chatroom & new mess ===============================================
 */
+   
--- src/chatclientBackend.js
+++ src/chatclientBackend.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 //import './chatclient.css';
 // import io from 'socket.io-client';
 /* import ScrollToBottom from 'react-scroll-to-bottom';
@@ -38,9 +39,13 @@ function MainContent() {
 
 // MainApp =======================================================================================================================
 function MainApp() {
+  const [apiUrl, setApiUrl ] = useState('');
   const [chatroom, setChatroom ] = useState('');
   const [userName, setUserName ] = useState('');
+  let [roomName, updateRoomName ] = useState('');
+
   useEffect(() => {
+    setApiUrl('http://localhost:3001');
     let subscription = chatRoom$.subscribe((chatRoom) => { 
       if (chatRoom) {
         setChatroom(chatRoom);
@@ -48,9 +53,17 @@ function MainApp() {
     });
   });
 
-
-  function setYourUserName() {
-
+  function creatRoom(){
+    console.log(roomName);
+    
+    axios.post(apiUrl + '/NewRoom', {roomName: roomName}, { 'Content-Type': 'application/json'}).
+      then((res) => {
+      console.log(res);
+      
+    });
+  }
+  let setRoomName = (e) => {
+    updateRoomName(e.target.value);
   }
   //if (this.state.redirect === true) return <Redirect to="/"/>;
   return (
@@ -63,20 +76,30 @@ function MainApp() {
           <HeaderContent/>
         </header>
         <main id="mainContainer">
-          <ChatRoom/>
-{/*           <Router>
-            <section id="gotToRoomContainer" style={(chatroom === '') ? {display: 'block'} : {display: 'none'}}>
-              <p id="chooseRoom">Välj rumm</p>
-              <section id="gotToRoom">
-                <Link className="button" to="/Chatroom1">Rum 1</Link>
-                <Link className="button" to="/Chatroom2">Rum 2</Link>
-                <Link className="button" to="/Chatroom3">Rum 3</Link>
+          {/* <ChatRoom/> */}
+          <Router>
+            <section id="goToRoomContainer" style={(chatroom === '') ? {display: 'block'} : {display: 'none'}}>
+              
+              <section id="createRoomContainer">
+                <p id="createRoomHl"> Skapa rum >>> </p> 
+                <span>
+                  <input type="text" onChange={ setRoomName }/> >>> <button className="button createRoomBtn" onClick={ creatRoom }> <p id="createRoomSymbol">+</p></button>  
+                </span> 
+              </section>
+              <section id="chooseRoomContainer">
+                <p id="chooseRoom">Välj rum:</p>
+                <section id="gotToRoom">
+
+  {/*                 <Link className="button" to="/Chatroom1">Rum 1</Link>
+                  <Link className="button" to="/Chatroom2">Rum 2</Link>
+                  <Link className="button" to="/Chatroom3">Rum 3</Link> */}
+                </section>
               </section>
             </section>
-              <Route exact path="/Chatroom1" component={ ChatRoom1 } />
+{/*               <Route exact path="/Chatroom1" component={ ChatRoom1 } />
               <Route exact path="/Chatroom2" component={ ChatRoom2 } />
-              <Route exact path="/Chatroom3" component={ ChatRoom3 } />
-            </Router> */}
+              <Route exact path="/Chatroom3" component={ ChatRoom3 } /> */}
+          </Router>
         </main>
       </>
   );
